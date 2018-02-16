15fe3d73a736240c6ba6c26d20966551ccd507a6
T-102 <T-102@PC259>
2018-02-15 19:43:39 -0600

    ArrayList

--- poo-jj/src/poliejemplo/Mascotas.java
+++ poo-jj/src/poliejemplo/Mascotas.java
@@ -5,13 +5,14 @@
  */
 package poliejemplo;
 
+import java.util.*;
 /**
  *
  * @author T-102
  */
 public class Mascotas {
     public static void main(String[] args){
-        ComportamientoAnimal animales[]=new [10];
+        Animal animales[]=new Animal[12];
         
         //Primero generamos a los gatos
         animales[0]=new Gato();
@@ -27,9 +28,26 @@ public class Mascotas {
         animales[8]=new Pollo();
         animales[9]=new Pollo();
         
+        animales[10]=new Raton();
+        animales[11]=new Raton();
+        
+        ServicioAnimales s=new ServicioAnimales();
+        
         //la magia del polimorfismo
-        for(ComportamientoAnimal animal:animales);
-        System.out.println(animal.hacerRuido());
+        for(Animal a: animales){
+        s.servicioHacerRuido((ComportamientoAnimal) a);
     }
-    
+        //genenrar un ArrayList
+    ArrayList<Animal> animalitos=new ArrayList<>();
+    animalitos.add(new Pollo());
+    animalitos.add(new Raton());
+    animalitos.add(new Raton());
+    animalitos.add(new Perro());
+    animalitos.add(new Gato());
+    for (Animal a: animalitos){
+        s.servicioHacerRuido((ComportamientoAnimal)a);
+    }
+    animalitos.get(0).setEdad(1);
+    System.out.println(animalitos.get(0).getEdad());
+    }  
 }
---
+++ poo-jj/src/poliejemplo/ServicioAnimales.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poliejemplo;
+
+/**
+ *
+ * @author T-102
+ */
+public class ServicioAnimales {
+    public void servicioHacerRuido(ComportamientoAnimal ca){
+        ca.hacerRuido();
+    }
+    
+}
